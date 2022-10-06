86064b5daf41d07f71166ab8c6e3908914185f1d
ldan2002 <ledaian22@gmail.com>
2022-10-06 21:05:25 +0700

    update home

--- online_shop/src/pages/Home.jsx
+++ online_shop/src/pages/Home.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import heroImg from '../assets/images/hero-img.png'
+import '../styles/home.css'
 
 const Home = () => {
+
+  const year = new Date().getFullYear()
+
   return (
-    <div>Home</div>
+    <>
+      <Helmet title={"Home"}></Helmet >
+      <section className="hero_section">
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className="hero_content">
+                <p className="hero_subtitle">Trending product in {year}</p>
+                <h2>Make Your More Minimalistic & Modern</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis molestias doloremque illum reiciendis qui, doloribus amet consectetur repudiandae officia maiores.</p>
+                <motion.button whileTap={{ scale: 1.2 }} className='buy_btn'>
+                  <Link to='/shop'>SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg='6' md='6'>
+              <div className="hero_img">
+                <img src={heroImg} alt="heroImg" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
